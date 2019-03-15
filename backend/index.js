24b0d540fb1414c2726ed421c287e2ef335fe6b0
Long Kim <1652728@hcmut.edu.vn>
2019-03-15 09:47:03 +0700

    Add index.js for back-end

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/', (req, res) => {
+  	res.send("Hello World.")
+});
+
+app.listen(3003, () =>
+  console.log('Express server is running on localhost:3003')
+);
