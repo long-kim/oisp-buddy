31575af6f32be240fb30bf262ce2d7d6c2502802
Long Kim <1652728@hcmut.edu.vn>
2019-03-17 04:43:35 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
-import logo from './hcmut.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./hcmut.svg";
+import "./App.css";
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    reponseToPost: ''
-  }
+    response: "",
+    post: "",
+    reponseToPost: ""
+  };
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({response: res.express}))
+      .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('api/get');
+    const response = await fetch("api/get");
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
 
     return body;
-  }
+  };
 
-  handleSubmit = async(e) => {
+  handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('api/post', {
-      method: 'POST',
+    const response = await fetch("api/post", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({post: this.state.post})
+      body: JSON.stringify({ post: this.state.post })
     });
 
     const body = await response.text();
 
-    this.setState({reponseToPost: body});
-  }
+    this.setState({ reponseToPost: body });
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo mb-3" alt="logo" />
-          <p>
-            OSIP Buddy Test Page
-          </p>
+          <p>OSIP Buddy Test Page</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -56,31 +54,42 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        <form className="row d-flex form-group mt-3 test-api mx-auto justify-content-center" onSubmit={this.handleSubmit}>
+        <form
+          className="row d-flex form-group mt-3 test-api mx-auto justify-content-center"
+          onSubmit={this.handleSubmit}
+        >
           <h2>Post to Server:</h2>
           <div className="col-md-9">
-            <input 
+            <input
               type="text"
-              className="form-control" 
+              className="form-control"
               placeholder="Send to server"
               value={this.state.post}
-              onChange={e => this.setState({post: e.target.value})}
-            >
-            </input>
+              onChange={e => this.setState({ post: e.target.value })}
+            />
           </div>
-            <div className="col-md-3 mt-md-0 mt-3">
-              <button 
-                type="submit"
-                className="btn btn-primary btn-block"
-              >
-                Submit
+          <div className="col-md-3 mt-md-0 mt-3">
+            <button type="submit" className="btn btn-primary btn-block">
+              <i className="fa fa-upload"></i>
+              &nbsp;Submit
             </button>
           </div>
         </form>
         <div className="row d-flex mt-3 mx-auto response test-api justify-content-center">
-          <div className={"alert alert-info " + (this.state.reponseToPost !== '' ? '' : 'd-none')} role="alert">
-            {this.state.reponseToPost.split('\n').map((item, key) => {
-              return <span key={key}>{item}<br></br></span>
+          <div
+            className={
+              "alert alert-info " +
+              (this.state.reponseToPost !== "" ? "" : "d-none")
+            }
+            role="alert"
+          >
+            {this.state.reponseToPost.split("\n").map((item, key) => {
+              return (
+                <span key={key}>
+                  {item}
+                  <br />
+                </span>
+              );
             })}
           </div>
         </div>
