58e894bd9cfadb0b9053cc7b622b7643fac668af
sarv19 <40037618+sarv19@users.noreply.github.com>
2019-04-13 17:49:41 +0700

    intialize

---
+++ src/frontend/components/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <h1> Hello World!</h1> );
+    }
+}
+ 
+export default Profile;
