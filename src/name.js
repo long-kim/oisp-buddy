fd202684748ddc80223dd2fe5af58f0daab00bbd
Duyken <44084147+Duyken@users.noreply.github.com>
2019-06-02 17:42:17 +0700

    Create name.js

---
+++ src/name.js
@@ -0,0 +1,15 @@
+export default `fugiat
+quia
+natus-soluta
+voluptas
+aut-illo
+non-quo
+officiis-sequi
+qui-sapiente
+accusamus-nostrum
+minus
+anng96
+jimcbl
+longkh
+sarah123
+duyly`.split('\n');
